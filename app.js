00b8714c8ac438a6735bb977df624d59add6e9df
Jacek Roszak <amigo9569@gmail.com>
2022-02-18 00:23:24 +0100

    menu item

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
 // bibliotek do błędów http
-var createError = require('http-errors');
-var express = require('express');
+var createError = require("http-errors");
+var express = require("express");
 // do pobierania public
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var path = require("path");
+var cookieParser = require("cookie-parser");
 // zrzucanie logów w trybie dev
-var logger = require('morgan');
+var logger = require("morgan");
 
 // importy routingów
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var newsRouter = require("./routes/news");
+var quizRouter = require("./routes/quiz");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 // parsowanie formularzy np.
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // deklaracja katalogu statycznego
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+// funkcja która będzie routem, będzie pobierała z requesta aktualny adres strony i go będziemy przekazywać do każdego naszego widoku, żeby puścić skrypt dalej wykorzystamy metode next. Jeżeli będę na /news najpierw wykona się ten routing a dzięki next przejdzie do zadeklarowanego routingu
+
+app.use(function (req, res, next) {
+  res.locals.path = req.path; //dzięki temu będzie globalnie dostępny w szablonach
+
+  next();
+});
 
 // deklaracja routingów
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/news", newsRouter);
+app.use("/quiz", quizRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 // wyłapywanie adresów które nie istnieją za pomocą błędów z biblioteki http errors
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
