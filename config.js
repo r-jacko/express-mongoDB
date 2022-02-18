b148d52b0a483b951354d7fbeb296531eddaa7fe
Jacek Roszak <amigo9569@gmail.com>
2022-02-18 18:26:44 +0100

    config edit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "mongodb+srv://admin:0DvrOxoZc7VLgdEq@cluster0.wspgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  db: "",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
